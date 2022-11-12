f8ba596e29e2ecf69baead461dc198625f87f31d
melxi <magomedf@ad.helsinki.fi>
2022-11-12 23:34:31 +0200

    Updated GridView and added weekly report

--- app/src/main/java/com/melxi/app/views/GridView.java
+++ app/src/main/java/com/melxi/app/views/GridView.java
@@ -1,37 +1,68 @@
 package com.melxi.app.views;
 
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class GridView {
-    private StackPane stackPane;
-    private int[][] cells = new int[12][12];
+    private GridPane gridPane;
+    private Cell[][] cells = new Cell[12][12];
 
     public GridView() {
-        stackPane = new StackPane();
-        stackPane.setMinSize(360, 360);
-        addBorders();
+        gridPane = new GridPane();
+        gridPane.setMinSize(360, 360);
+        addCells();
+        setSourceCell(1, 1);
+        setDestinationCell(10, 10);
     }
 
-    private void addBorders() {
+    public void addCells() {
         for (int row = 0; row < cells.length; row++){
             for (int col = 0; col < cells[row].length; col++){
-                Rectangle rect = new Rectangle();
-                rect.setWidth(30);
-                rect.setHeight(30);
-                rect.setFill(Color.TRANSPARENT);
-                rect.setStroke(Color.BLACK);
+                Cell rect = new Cell();
 
-                rect.setTranslateX((col * 30) - 260);
-                rect.setTranslateY((row * 30) - 160);
+                GridPane.setRowIndex(rect.getRectangle(), row);
+                GridPane.setColumnIndex(rect.getRectangle(), col);
 
-                stackPane.getChildren().add(rect);
+                gridPane.getChildren().addAll(rect.getRectangle());
+
+                cells[row][col] = rect;
             }
         }
     }
 
-    public StackPane getGridView() {
-        return stackPane;
+    public Cell getCell(int x, int y) {
+        return cells[x][y];
+    }
+
+    public void setSourceCell(int row, int col) {
+        Cell cell = getCell(row, col);
+
+        cell.getRectangle().setFill(Color.GREEN);
+    }
+
+    private void setDestinationCell(int row, int col) {
+        Cell cell = getCell(row, col);
+
+        cell.getRectangle().setFill(Color.RED);
+    }
+
+    public GridPane getGridView() {
+        return gridPane;
+    }
+
+    private class Cell {
+        private Rectangle rect;
+        public Cell() {
+            rect = new Rectangle();
+            rect.setWidth(30);
+            rect.setHeight(30);
+            rect.setFill(Color.TRANSPARENT);
+            rect.setStroke(Color.BLACK);
+        }
+
+        public Rectangle getRectangle() {
+            return rect;
+        }
     }
 }
--- docs/reports.md
+++ docs/reports.md
@@ -2,3 +2,6 @@
 
 ## Week 1
 This week I have spent mostly writing the design document and learning the pathfinding algorithms which are in comparison, namely Dijkstra's algorithm and Iterative Deepening A* (IDA*). I also spent time learning the basics of graphic framework JavaFX which will be used for visualization.
+
+## Week 2
+Designed and implemented initial user interface for the application. So far the user interface has a grid representing graph with source and destination nodes. Below the grid there are two buttons which purpose is to start the algorithms for finding shortest paths. Next week I will start implementing Dijkstra's algorithm and visualising it.
